b9418e3705cb420a1dee375e771208484c6335b2
João Pedro <jp.pedrozo@hotmail.com>
2025-07-01 02:16:21 -0300

    feat: add notas tab

---
+++ app/notas/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NotasPage() {
+  return (
+    <div className="flex items-center justify-center min-h-[60vh]">
+      {/* Texto vazio para ser alterado depois */}
+      <span>
+        Notas
+      </span>
+    </div>
+  );
+} 
